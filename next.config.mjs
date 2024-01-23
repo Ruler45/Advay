1d9243276795b06905a469b4411548719d7c9a12
ruler45 <sahin0945@gmail.com>
2024-01-23 12:17:56 +0530

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+// const nextConfig = {};
+
+// const withPWA =
+//   process.env.NODE_ENV === "production"
+//     ? require("next-pwa")({
+//         dest: "public",
+//       })
+//     : {};
+
+// exports = {
+//   ...nextConfig,
+//   ...withPWA,
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   }
+// };
